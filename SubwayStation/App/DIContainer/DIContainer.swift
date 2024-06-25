ba1ae1286f2ad573b535d27d64c279a656f29099
kiseok <carti1108@naver.com>
2024-06-25 18:04:12 +0900

    feat: register 및 resolve 메서드 수정

--- SubwayStation/App/DIContainer/DIContainer.swift
+++ SubwayStation/App/DIContainer/DIContainer.swift
@@ -12,13 +12,33 @@ final class DIContainer {
     
     private var dependencies: [String: Any] = .init()
     
-    func register<T>(_ dependency: T, for type: T.Type, key: String = "") {
+    private init() {}
+    
+    //MARK: register
+    func register<T>(_ type: T.Type, factory: @escaping () -> T, key: String = "") {
         let key = key.isEmpty ? String(describing: type) : key
-        self.dependencies[key] = dependency
+        self.dependencies[key] = factory
     }
     
+    func register<T, Arg>(_ type: T.Type, key: String = "", factory: @escaping (Arg) -> T) {
+        let key = key.isEmpty ? String(describing: type) : key
+        self.dependencies[key] = factory
+    }
+    
+    //MARK: resolve
     func resolve<T>(_ type: T.Type, key: String = "") -> T? {
         let key = key.isEmpty ? String(describing: type) : key
-        return dependencies[key] as? T
+        guard let factory = dependencies[key] as? () -> T else {
+            return nil
+        }
+        return factory()
+    }
+    
+    func resolve<T, Arg>(_ type: T.Type, argument: Arg, key: String = "") -> T? {
+        let key = key.isEmpty ? String(describing: type) : key
+        guard let factory = dependencies[key] as? (Arg) -> T else {
+            return nil
+        }
+        return factory(argument)
     }
 }
