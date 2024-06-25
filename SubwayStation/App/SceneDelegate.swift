7ee12175e7ec71abcb7f7c36cca5589ae2955fb1
kiseok <carti1108@naver.com>
2024-06-25 18:06:43 +0900

    chore: 필요없는 주석 제거

--- SubwayStation/App/SceneDelegate.swift
+++ SubwayStation/App/SceneDelegate.swift
@@ -15,8 +15,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let navigationController: UINavigationController = .init()
         
-//        di()
-        
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = navigationController
         window?.backgroundColor = .systemBackground
@@ -25,29 +23,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         appCoordinator.start()
         window?.makeKeyAndVisible()
     }
-    
-//    func di() {
-//        //Provider
-//        DIContainer.shared.register(Provider(),
-//                                    for: Providable.self)
-//        let provider = DIContainer.shared.resolve(Providable.self)!
-//
-//        //Repositories
-//        DIContainer.shared.register(StationSearchRepositoryImpl(provider: provider),
-//                                    for: StationSearchRepository.self)
-//        DIContainer.shared.register(StationArrivalRepositoryImpl(provider: provider),
-//                                    for: StationArrivalRepository.self)
-//        let stationSearchRepositoryImpl = DIContainer.shared.resolve(StationSearchRepository.self)!
-//        let stationArrivalRepositoryImpl = DIContainer.shared.resolve(StationArrivalRepository.self)!
-//        
-//        //UseCases
-//        DIContainer.shared.register(StationSearchUseCase(stationRepository: stationSearchRepositoryImpl),
-//                                    for: StationSearchUseCase.self)
-//        DIContainer.shared.register(StationArrivalUseCase(repository: stationArrivalRepositoryImpl),
-//                                    for: StationArrivalUseCase.self)
-//        
-//        //ViewModel
-//
-//    }
 }
 
